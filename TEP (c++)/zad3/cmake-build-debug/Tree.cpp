fa60af9286a8b62f1933898084cd60ffb599fb8f
0leslaw <272736@student.pwr.edu.pl>
2023-11-11 14:11:14 +0100

    3

--- TEP (c++)/zad3/cmake-build-debug/Tree.cpp
+++ TEP (c++)/zad3/cmake-build-debug/Tree.cpp
@@ -4,13 +4,56 @@
 
 #include "Tree.h"
 
-Tree::Node::Node(std::string value) {
-    this->value = value;
+Tree::Node::Node(std::string& value) {
+    this->value = &value;
     this->left = NULL;
     this->right = NULL;
 }
 
-bool Tree::setupTree(std::string) {
+Tree::Node* Tree::Node::getLeft() {
+    return left;
+}
 
-    return false;
+Tree::Node* Tree::Node::getRight() {
+    return right;
+}
+
+bool Tree::setupTree(std::string& value) {
+    int* currentIndex = new int(0);
+
+    return setupTreeHelper(currentIndex,value, root);
+}
+
+bool Tree::setupTreeHelper(int *currentIndex, std::string &value, Node* root) {
+    //current is either a constant
+    if(value[*currentIndex] > 47 && value[*currentIndex] < 58){
+        //we just have to find where the number ends to continue
+        int firstIndex = *currentIndex;
+        *currentIndex++;
+        while (value[*currentIndex] > 47 && value[*currentIndex] < 58)
+            *currentIndex++;
+
+        root->setValue(value.substr(firstIndex,*currentIndex));
+    }
+    //or its a variable
+    if(value[*currentIndex] > 96 && value[*currentIndex] < 123){
+        //we just have to find where the number ends to continue
+        int firstIndex = *currentIndex;
+        *currentIndex++;
+        while (value[*currentIndex] > 47 && value[*currentIndex] < 58)
+            *currentIndex++;
+
+        root->setValue(value.substr(firstIndex,*currentIndex));
+    }
+    //OR its an operator with two variables
+    if(value[*currentIndex] == 42
+    || value[*currentIndex] == 43
+    || value[*currentIndex] == 45
+    || value[*currentIndex] == 47
+    ){
+        //we just have to find where the number ends to continue
+        root->setValue(value.substr(*currentIndex,*currentIndex));
+        *currentIndex++;
+
+    }
 }
--- TEP (c++)/zad3/cmake-build-debug/Tree.h
+++ TEP (c++)/zad3/cmake-build-debug/Tree.h
@@ -6,23 +6,30 @@
 #define ZAD3_TREE_H
 #import <iostream>
 #include <wsman.h>
-
+using namespace std;
 class Tree {
 private:
     class Node{
     private:
-        std::string value;
-        Node left;
-        Node right;
+        std::string* value;
+        Node* left;
+        Node* right;
     public:
-        Node(std::string value);
+        Node(std::string& value);
+        Node* getLeft();
+        Node* getRight();
+        string* getValue();
+        void setLeft(Node& node);
+        void setRight(Node& node);
+        void setValue(string s);
+
     };
-    Node root;
+    Node* root;
 
 public:
     Tree();
-    bool setupTree(std::string);
-    
+    bool setupTree(std::string& value);
+    bool setupTreeHelper(int* currentIndex, std::string& value, Node* root);
 };
 
 
