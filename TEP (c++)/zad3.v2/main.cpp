abc88138ead8ea77568223bdefa7b8b48f05bbc0
0leslaw <119787710+0leslaw@users.noreply.github.com>
2023-12-11 21:10:00 +0100

    Revert "scala i cpp"

--- TEP (c++)/zad3.v2/main.cpp
+++ TEP (c++)/zad3.v2/main.cpp
@@ -1,24 +1,9 @@
-#include <iostream>
-#include <cstdlib>
+
 #include "ConsoleInterface.h"
-template <typename T>
-void runConsoleInterface() {
-    ConsoleInterface<T> consoleInterface;
-    consoleInterface.run();
-}
 
 int main() {
 
-    string driver;
-    cout<< "Wybierz\n1 - int,\n2 - double lub\n3 - string,\n cokolwiek innego - wyjscie: ";
-    std::getline(std::cin, driver);
-    if(driver == "1")
-            runConsoleInterface<int>();
-    if(driver == "2")
-            runConsoleInterface<double>();
-    if(driver == "3")
-            runConsoleInterface<string>();
-
-
+    ConsoleInterface consoleInterface;
+    consoleInterface.run();
     return 0;
 }
