c36e78a1c7f27a54a6adb114913bf1028c4747fb
0leslaw <272736@student.pwr.edu.pl>
2023-12-11 21:04:28 +0100

    scala i cpp

--- PP (scala, OCalm/scala/zad 1 scala/src/main/scala/zadania/Laby3XD.scala
+++ PP (scala, OCalm/scala/zad 1 scala/src/main/scala/zadania/Laby3XD.scala
@@ -24,4 +24,49 @@ object Laby3XD {
     }
     helper(ListX)((ListX.head,0))
   }
+
+
+
+  //LABY 4 ZADANKO NA LEKCJI
+  def sum(i1:Int,i2:Int):Int = {
+    i1+i2
+  }
+  def accumulateClassic (list : List[Int])(entryState:Int)(f: (Int,Int)=>Int):Int = {
+    def accumulateHelper(list: List[Int]): Int= list match {
+      case Nil => 0
+      case head :: tail => f(head,accumulateHelper(tail))
+    }
+
+    accumulateHelper(list)+entryState
+  }
+
+  def accumulateTail(list: List[Int])(entryState: Int)(f: (Int, Int) => Int): Int = {
+    def accumulateTailHelper(list: List[Int])(subScore:Int): Int = list match {
+      case Nil => subScore
+      case head :: tail => accumulateTailHelper(tail)(f(subScore,head))
+    }
+
+    accumulateTailHelper(list)(entryState)
+  }
+  def sposobPrzeksztalcenia (f1:Double,f2:Double):(Double,Double) = {
+    (f1+2,f2+1)
+  }
+  def przeksztalcenie(list : List[(Double,Double)])(f:(Double,Double)=>(Double,Double)): List[(Double,Double)]=list match {
+    case Nil => List[(Double,Double)]()
+    case head::tail => (f (head._1,head._2))::przeksztalcenie(tail)(f)
+  }
+
+  def lrepeat[A](k: Int)(lxs: LazyList[A]): LazyList[A] = {
+    def loop (h: A)(i:Int):LazyList[A]  = {
+      if(k<i){
+        h #:: loop(h)(i+1)
+      }else
+        LazyList()
+    }
+    lxs match {
+      case h #:: LazyList() => loop (h)(0)
+      case (h:A) #:: (t:LazyList[A]) => loop (h)(0) #::: lrepeat(k)(t)
+    }
+  }
+
 }
